96fff809327e7062691e845ca26af483a5d3df11
Alleaxx <32822043+Alleaxx@users.noreply.github.com>
2022-05-03 17:35:15 +0300

    Перевод старых румайностей 2015-2018 в веб-формат и обновление ссылок

--- Румайности/Versions.js
+++ Румайности/Versions.js
@@ -2,8 +2,8 @@ let nav = document.querySelector('#newspapers');
 let navOld = document.querySelector('#newspapers-old');
 
 let names = [
-    { type: 'old',          name : 'Румайности 2014, классика' },
-    { type: 'retro-old',    name : 'Эпизоды Румайностей' },
+    { type: 'old',          name : 'Румайности 2014' },
+    { type: 'retro-old',    name : 'Эпизоды Румайностей 2013' },
     { type: 'old-2015',     name : 'Румайности 2015' },
     { type: 'old-2016',     name : 'Румайности 2016-2018' },
     { type: 'old-2017',     name : 'Румайности 2017' },
@@ -14,36 +14,37 @@ let versions = [
     { type: 'retro-old', name: '01.01.13', href: '../2013/01.01.13.html' },
     { type: 'retro-old', name: '26.01.13', href: '../2013/26.01.13.html' },
 
-    { type: 'old', name: '07.07.14', href: '../2014/07.07.14.html' },
-    { type: 'old', name: '08.07.14', href: '../2014/08.07.14.html' },
-    { type: 'old', name: '09.07.14', href: '../2014/09.07.14.html' },
-    { type: 'old', name: '10.07.14', href: '../2014/10.07.14.html' },
-    { type: 'old', name: '11.07.14', href: '../2014/11.07.14.html' },
-    { type: 'old', name: '12.07.14', href: '../2014/12.07.14.html' },
-    { type: 'old', name: '13.07.14', href: '../2014/13.07.14.html' },
+    { type: 'old', name: '07.07.14', href: '../2014/14.07.07.html' },
+    { type: 'old', name: '08.07.14', href: '../2014/14.07.08.html' },
+    { type: 'old', name: '09.07.14', href: '../2014/14.07.09.html' },
+    { type: 'old', name: '10.07.14', href: '../2014/14.07.10.html' },
+    { type: 'old', name: '11.07.14', href: '../2014/14.07.11.html' },
+    { type: 'old', name: '12.07.14', href: '../2014/14.07.12.html' },
+    { type: 'old', name: '13.07.14', href: '../2014/14.07.13.html' },
     { type: 'old', name: '14.07.14', href: '../2014/14.07.14.html' },
 
-    { type: 'old-2015', name: '25.03.15', href: '../2015/25.03.15.html' },
-    { type: 'old-2015', name: '26.03.15', href: '../2015/26.03.15.html' },
-    { type: 'old-2015', name: '27.03.15', href: '../2015/WIP 27.03.15.html' },
-    { type: 'old-2015', name: '28.03.15', href: '../2015/WIP 28.03.15.html' },
-    { type: 'old-2015', name: '29.03.15', href: '../2015/WIP 29.03.15.html' },
-    { type: 'old-2015', name: '30.03.15', href: '../2015/WIP 30.03.15.html' },
-    { type: 'old-2015', name: '31.03.15', href: '../2015/WIP 31.03.15.html' },
-    { type: 'old-2015', name: '01.04.15', href: '../2015/WIP 01.04.15.html' },
-    { type: 'old-2015', name: '02.04.15', href: '../2015/WIP 02.04.15.html' },
+    { type: 'old-2015', name: '25.03.15', href: '../2015/15.03.25.html' },
+    { type: 'old-2015', name: '26.03.15', href: '../2015/15.03.26.html' },
+    { type: 'old-2015', name: '27.03.15', href: '../2015/15.03.27.html' },
+    { type: 'old-2015', name: '28.03.15', href: '../2015/15.03.28.html' },
+    { type: 'old-2015', name: '29.03.15', href: '../2015/15.03.29.html' },
+    { type: 'old-2015', name: '30.03.15', href: '../2015/15.03.30.html' },
+    { type: 'old-2015', name: '31.03.15', href: '../2015/15.03.31.html' },
+    { type: 'old-2015', name: '01.04.15', href: '../2015/15.04.01.html' },
+    { type: 'old-2015', name: '02.04.15', href: '../2015/15.04.02.html' },
 
-    { type: 'old-2016', name: '12.04.16', href: '../2016/WIP 12.04.16.html' },
-    { type: 'old-2016', name: '10.06.16', href: '../2016/WIP 10.06.16.html' },
-    { type: 'old-2016', name: '06.07.16', href: '../2016/WIP 06.07.16.html' },
-    { type: 'old-2016', name: '07.07.16', href: '../2016/WIP 07.07.16.html' },
-    { type: 'old-2016', name: '08.07.16', href: '../2016/WIP 08.07.16.html' },
+    { type: 'old-2016', name: '12.04.16', href: '../2016/16.04.12.html' },
+    { type: 'old-2016', name: '10.06.16', href: '../2016/16.06.10.html' },
+    { type: 'old-2016', name: '06.07.16', href: '../2016/16.07.06.html' },
+    { type: 'old-2016', name: '07.07.16', href: '../2016/16.07.07.html' },
+    { type: 'old-2016', name: '08.07.16', href: '../2016/16.07.08.html' },
 
-    { type: 'old-2016', name: '28.05.17', href: '../2017/WIP 28.05.17.html' },
-    { type: 'old-2016', name: '19.10.18', href: '../2018/WIP 19.10.18.html' },
+    { type: 'old-2016', name: '28.05.17', href: '../2017/17.05.28.html' },
+    { type: 'old-2016', name: '19.10.18', href: '../2018/18.10.19.html' },
 
     { type: 'new', name: '2019 год', href: '../2019/21.10.19.html' },
     { type: 'new', name: '2020 год', href: '../2020/21.10.20.html' },
+    // { type: 'new', name: '2021 год', href: '../2021/2021.html' },
 ];
 
 let uls = {};
@@ -58,8 +59,8 @@ versions.forEach(v => {
     let add = '';
     if(current && current.getAttribute('data-date') === v.name){
         add = 'current-link';
-        console.log('Ага, попался!');
-        console.log(v);
+        // console.log('Ага, попался!');
+        // console.log(v);
     }
 
     li.innerHTML = `<a class="${add}" href="${v.href}">${v.name}</a>`;
